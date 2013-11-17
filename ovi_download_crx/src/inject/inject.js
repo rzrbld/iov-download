73e1da083782d08cbeaf69fe6cf2d2d82fa28bbb
rzrbld <razblade@gmail.com>
2013-11-17 06:47:15 +0400

    initial porting
    port to Opera 15 and later, and Chrome

---
+++ ovi_download_crx/src/inject/inject.js
@@ -0,0 +1,15 @@
+window.onload = startScript;
+
+function startScript(){
+	console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+    var getULlist=document.getElementsByTagName('UL');
+    for(var i=0;i<getULlist.length;i++){
+        if(getULlist[i].className=='contentActions'){
+            var DownloadButton = document.createElement('LI');
+            DownloadButton.innerHTML='<a href="http://'+window.location.hostname+''+window.location.pathname+'/download" class="btnGraphic btnDownload">'+
+            '<span class="overflow" style="padding-left:14px;padding-top:11px;width:auto!important;margin-right:30px;height:25px;" title="Download" >'+
+            'Download.</span></a>';
+            getULlist[i].insertBefore(DownloadButton, getULlist[i].firstChild);
+        }
+    }
+}
